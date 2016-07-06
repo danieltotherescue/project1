c1a457f831ad4fdbb2d351cf0a0811d30e27d8e5
Daniel Landau <danieltotherescue@gmail.com>
2016-07-06 12:36:59 -0700

    question functionality is working hooray

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ console.log("connected")
 //referred to alot - makes workflow easier
 
 // document.getElementById('quizSection').innerHTML= "Are you ready to begin the game? Click to begin!";
-//
+
 // document.getElementById('quizSection').addEventListener('click', function(){
-//
-//   // document.getElementById('quizSection').innerHTML= "";
+// document.getElementById('quizSection').innerHTML= "";
 // });
 
 //defining intial elements
@@ -19,103 +18,135 @@ console.log("connected")
 var questions = [
   {
     prompt: "How many chickens are consumed in the US each year?",
-    corrAns: "8 billion",
+    corrAns: "a2",
     answers: ["13 million", "8 billion", "90 million", "10 billion"]
   },
   {
     prompt: "According to a UM Study, what percent of adults lie at least once in a 10 minutes conversation?",
-    corrAns: "60%",
+    corrAns: "a2",
     answers: ["85%", "60%", "40%", "10%"]
 
 }, {
     prompt: "How many buses are in the Santa Monica blue bus fleet (as of 2002)?",
-    corrAns: "196",
+    corrAns: "a2",
     answers: ["315", "196", "240", "119"]
 
 }, {
     prompt: "Who originally created MS Dos (the system on which the Microsoft OS was based)?",
-    corrAns: "Tim Paterson",
+    corrAns: 'a1',
     answers: ["Tim Paterson", "Sergei Brin", "Allen Yankovich", "Ahriman Patel"]
 
 }, {
     prompt: "Why are there only 40 songs in the top 40?",
-    corrAns: "The jukeboxes at the time could only contain 40 songs",
+    corrAns: "a2",
     answers: ["40 was the CEO of Billboard’s favorite number", "The jukeboxes at the time could only contain 40 songs", "40 is the loneliest number that you’ll ever do", "The periodical pages on which the list was originally printed were formatted to take a maximum of 40 entries"]
 
 }, {
     prompt: "In Shakespeare’s time, the term 'thou' was used:",
-    corrAns: "to express intimacy, familiarity or even disrespect",
-    answers: ["as a formal greeting", "to express intimacy, familiarity or even disrespect", "as an explative", "to indicate a numerical value"]
+    corrAns: "a3",
+    answers: ["as a formal greeting", "as an explative", "to express intimacy, familiarity or even disrespect", "to indicate a numerical value"]
 
 }, {
     prompt: "Which of the following is true?",
-    corrAns: "Tenants in Arkansas can be jailed for being one day late with rent",
-    answers: ["Tenants in Arkansas can be jailed for being one day late with rent", "The inventor of the lobotomy first proved its effectiveness by self administering one.", "Legos were first invented in Norway", "Stanley Kubrick’s real name is Mark Zuckerberg", "Mark Zuckerberg’s real name is Stanley Kubrick"]
+    corrAns: "a3",
+    answers: ["The inventor of the lobotomy first proved its effectiveness by self administering one.", "Legos were first invented in Norway", "Tenants in Arkansas can be jailed for being one day late with rent", "Stanley Kubrick’s real name is Mark Zuckerberg", "Mark Zuckerberg’s real name is Stanley Kubrick"]
 
 }, {
     prompt: "Actor Bryan Cranston of Breaking Bad has voiced villains on which popular TV show?",
-    corrAns: "Power Rangers",
+    corrAns: "a2",
     answers: ["Batman the Animated Series", "Power Rangers", "Pokemon", "Teenage Mutant Ninja Turtles"]
 
 }, {
     prompt: "Elvis Presley collected which of the following in every city in which he performed?",
-    corrAns: "Police Badges",
-    answers: ["Peanut Butter and Banana sandwiches", "Police Badges", "Concert Tickets", "A carton of eggs"]
+    corrAns: "a3",
+    answers: ["Peanut Butter and Banana sandwiches", "Concert Tickets", "Police Badges", "A carton of eggs"]
 
 }, {
     prompt: "Before his role in American Graffitti, Harrison Ford worked as a:",
-    corrAns: "Carpenter",
-    answers: ["Plumber", "Garbage Collector", "Carpenter", "Bus driver"]
+    corrAns: "a4",
+    answers: ["Plumber", "Garbage Collector", "Bus driver", "Carpenter"]
 
 }, {
     prompt: "What is the percentage decrease in air quality throughout the US on July 4th as a result of fireworks displays?",
-    corrAns: "42%",
+    corrAns: "a3",
     answers: ["100%", "84%", "42%", "21%"]
 
 }]
 
 
 
-//set =var and splice out to question.splice to remove array so it doesn't repeat
+
 //currentQ
+
+
 
 //for loop to go through answers and create buttons for each
 // for (var i=0; i<answers.length; i++) {
 //
 // }
 
-//
 
-var randQuestion = function () {
-  return questions.splice(Math.floor(Math.random() * questions.length), 1)[0];
-}
+// var randQuestion = function () {
+//   return questions.splice(Math.floor(Math.random() * questions.length), 1)[0];
+// }
+var correctAnswer;
+var currentAnswers;
+
 
 //brute force/one by one approach
-$('#prompt').html(randQuestion);
-// $('#answers').html('Question 1 - what is your favorite color?');
+// $('#prompt').html(randQuestion);
 
-$('#nextQ').on('click', function(){
-  var newObject = questions.splice(Math.floor(Math.random() * questions.length), 1)[0];
-  var newQuestion = newObject.prompt;
-  var newAnswer = newObject.answers;
-  $('#instructions').html("");
-  $('#prompt').html(newQuestion);
-  $('#a1').html(newAnswer[0]);
-  $('#a2').html(newAnswer[1]);
-  $('#a3').html(newAnswer[2]);
-  $('#a4').html(newAnswer[3]);
 
-  // song.play();
-  // song.loop();
-})
-//maybe shorter with an if statement if extra time
-$('#a1').hover(function(){
-  $(this).css("background", "dodgerblue");
+$('#nextQ').click(function(){
+  console.log("Is working?");
+  $('.answers').append('<div class="selectors" id="a1"></div>', '<div class="selectors" id="a2"></div>', '<div class="selectors" id="a3"></div>', '<div class="selectors" id="a4"></div>');
+  $('#nextQ').hide();
+
+  startGame();
+    // create & append div with score
 })
 
-$('#a1').mouseout(function(){
-  $(this).css("background", "white");
-})
+function startGame() {
+  nextQuestion();
+
+  function nextQuestion(){
+    var newObject = questions.splice(Math.floor(Math.random() * questions.length), 1)[0];
+    var newQuestion = newObject.prompt;
+    var newAnswer = newObject.answers;
+    $('#instructions').html("");
+    $('#prompt').html(newQuestion);
+    $('#a1').html(newAnswer[0]);
+    $('#a2').html(newAnswer[1]);
+    $('#a3').html(newAnswer[2]);
+    $('#a4').html(newAnswer[3]);
+    correctAnswer = newObject.corrAns;
+  }
+
+  $('.selectors').on('click', function(){
+    if  (this.id == correctAnswer){
+      alert('You are correct!');
+      nextQuestion();
+      //tally correct answers
+      $('#tally').html();
+      //no drawing on the hangin' pole
+    }else {
+      alert("That's Wrong, baby.");
+      //draw a section of the man
+      nextQuestion();
+    }
+  })
+
+  //mouseover choice colors - maybe shorten with an if statement if extra time
+  $('#a1, #a2, #a3, #a4').hover(function(){
+    $(this).css("background", "lightgray");
+  })
+
+  $('#a1, #a2, #a3, #a4').mouseout(function(){
+    $(this).css("background", "white");
+  })
+
+}
+
 
 //reference section
 
@@ -124,6 +155,12 @@ $('#a1').mouseout(function(){
 // })
 
 
+//create array of user answers to generate final result
+
 
 // array of jpeg elements of the manthat will be referenced and loaded on each incorrect answer submission click
 //create function to generate div by altering or adding html in that div
+
+//for music if I have time later
+// song.play();
+// song.loop();
